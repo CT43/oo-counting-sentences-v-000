0a663f586ee7ac96b73fbdad3c54a18ddd327ed1
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-26 22:29:53 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split('.').join("?").split("?").join("!").split("!").compact.size
+    self.split('.').join("?").split("?").join("!").split("!").compact.flatten.size
 
   end
 end
